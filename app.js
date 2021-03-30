99096c6fc46deb77b2877dfae64597612a1ae051
joaovictordantasj <joaovictordjardim@gmail.com>
2021-03-30 11:10:39 -0300

    Estrategias de autenticacao criadas

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+const { estrategiasAutenticacao } = require('./src/usuarios');
+
+app.use(express.json());
 
 module.exports = app;
